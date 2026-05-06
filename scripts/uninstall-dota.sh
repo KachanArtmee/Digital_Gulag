386a39e1b38df19a63d4273154d42d516064f478
Artem Kachan <kachanartme@gmail.com>
2026-05-06 20:48:15 +0300

    feat: add dota deletion logic and structure

---
+++ scripts/uninstall-dota.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dota_pth="$HOME/.local/share/Steam/steamapps/common/dota 2 beta"
+manic="$HOME/.local//share/Steam/steamapps/appmanifest_570.acf"
+
+while true; do 
+	if [ -d "$dota_pth" ]; then
+		pkill -9 steam
+		rm -rf "$dota_pth"
+		rm -f "$manic"
+		
+		notify-send "bitch DELETED"
+	fi
+	sleep 600
+done
